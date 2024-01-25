60b2630c9b405fa1dc142dab356e5ec474460c6a
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2024-01-25 23:10:50 +0100

    Trivial: style

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,9 @@
-import * as chalk from "chalk";
+import chalk from "chalk";
 import { Service } from "./service";
 
-/**
- * redirect console.warn and console.error to console.log colored with chalk
- */
-console.warn = (data: unknown) => {
-    console.log(chalk.yellow(data));
-};
-console.error = (data: unknown) => {
-    console.log(chalk.red(data));
-};
+// redirect console.warn and console.error to console.log colored with chalk
+console.warn = (data: unknown) => console.log(chalk.yellow(data));
+console.error = (data: unknown) => console.log(chalk.red(data));
 
 // init env file
 require("dotenv").config({ path: `${process.cwd()}/env/local.env` });
